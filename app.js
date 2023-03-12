760ac5949c70e4ba9506b0c104111a8e1a8d016b
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-03-12 09:31:23 +0530

    username and chat appended

--- app.js
+++ app.js
@@ -18,12 +18,18 @@ app.use('/chat',(req,res,next)=>{
     let chat = req.body;
     Object.keys(chat).forEach(val=>{
         console.log(chat[val]);
-        fs.readFile('chat.txt', 'utf8', function(err, data){
+        // fs.readFile('chat.txt', 'utf8', function(err, data){
+        //     if(err){
+        //         console.log(err);
+        //         return err;
+        //     }
+        //     fs.writeFileSync("chat.txt",data+chat[val]);
+        // });
+        fs.appendFile("chat.txt",chat[val]+",",err => {
             if(err){
-                console.log(err);
-                return err;
+                throw err;
             }
-            fs.writeFileSync("chat.txt",data+chat[val]);
+            console.log("updated");
         });
     })
     res.redirect("/");
@@ -34,7 +40,13 @@ app.use('/',(req,res,next)=>{
     // console.log(localStorage.getItem("username"));
     let chat = req.body;
     Object.keys(chat).forEach(val=>{
-        fs.writeFileSync("chat.txt",chat[val]+": ");
+        // fs.writeFileSync("chat.txt",chat[val]+": ");
+        fs.appendFile("chat.txt",chat[val]+": ",err => {
+            if(err){
+                throw err;
+            }
+            console.log("updated");
+        });
         console.log(chat[val]);
     })
     res.send('<form action="/chat" method="POST"><input type="text" name="chat" placeholder="type something..."><button>Send</button></form>')
